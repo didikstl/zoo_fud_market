1c93f1a438fe9d661a2c8eb81e4359fb42174ca4
didikstl <125580027+didikstl@users.noreply.github.com>
2023-10-17 18:30:18 +0300

    order

--- apps/order/models.py
+++ apps/order/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from apps.catalog.models import Product
 from apps.user.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 
 class Cart(models.Model):
@@ -14,3 +16,18 @@ class Cart(models.Model):
         verbose_name_plural = 'Корзины'
 
 
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name='Покупатель', on_delete=models.CASCADE)
+    total = models.DecimalField(verbose_name='Итого', max_digits=12, decimal_places=2)
+    first_name = models.CharField(verbose_name='Имя', max_length=255)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=255)
+    email = models.EmailField(verbose_name='E-mail')
+    phone = PhoneNumberField(verbose_name='Телефон')
+    address = models.TextField(verbose_name='Адрес')
+    comment = models.TextField(verbose_name='Коментарий', null=True, blank=True)
+    created_ad = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_ad = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
